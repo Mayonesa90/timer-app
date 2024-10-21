d939d9d794cac26dd3f82c71d41b40554bb2e78e
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-10-21 21:26:55 +0200

    added some animations to button, arrows, menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {motion, useAnimate, AnimatePresence} from 'framer-motion'
 
 import LogoHorizontalBlack from '../assets/logoHorizontalBlack.svg'
 import LogoHorizontalWhite from '../assets/logoHorizontalWhite.svg'
@@ -7,14 +8,34 @@ import LogoHorizontalWhite from '../assets/logoHorizontalWhite.svg'
 export default function Menu(){
 
     const [menuOpen, setMenuOpen] = useState(false) 
+    // const [scope, animate] = useAnimate()
 
     function toggleMenu(){
         setMenuOpen(!menuOpen)
     }
 
-    if (menuOpen){
-        return (
-            <nav className='min-w-[375px] h-full bg-gray-900 grid items-center justify-center absolute'>
+    const menuVariants = {
+        open: {
+            opacity: 1,
+            transition: { duration: 0.35 }
+        },
+        closed: {
+            opacity: 0,
+            transition: { duration: 0.35 }
+        }
+    }
+
+    return (
+        <AnimatePresence>
+        {menuOpen ? (
+            <motion.nav 
+                className='menu-open min-w-[375px] h-full bg-gray-900 grid items-center justify-center absolute'
+                key={'menu-open'}
+                initial='closed'
+                animate='open'
+                exit='closed'
+                variants={menuVariants}
+                >
                 <img src={LogoHorizontalWhite} onClick={toggleMenu}alt="logo" className='w-9 pt-3 ml-4 absolute top-3' />
                 <ul className=' font-PTSans tracking-widest font-bold text-2xl text-gray-50 flex flex-col gap-8 '>
                     <li>ANALOG TIMER</li>
@@ -23,15 +44,56 @@ export default function Menu(){
                     <li>TEXT TIMER</li>
                     <li>CIRCLES TIMER</li>
                 </ul>
-            </nav>
+            </motion.nav>
+        ) : (
+            <motion.nav 
+            className='menu-closed min-w-[375px] bg-gray-50 shadow-2xl absolute top-3'
+            key={'menu-closed'}
+            initial='closed'
+            animate='open'
+            exit='closed'
+            variants={menuVariants}
+            >
+            <img src={LogoHorizontalBlack} onClick={toggleMenu} alt="logo" className='w-9 pt-3  ml-4 absolute ' />
+            <h1 className=' tracking-widest text-gray-400 text-xl font-RighteousFont absolute top-3 left-[40%]'>interval</h1>
+            </motion.nav>
         )
-    } else {
-        return (
-            <nav className='min-w-[375px] bg-gray-50 shadow-2xl absolute top-3'>
-                <img src={LogoHorizontalBlack} onClick={toggleMenu} alt="logo" className='w-9 pt-3  ml-4 absolute ' />
-                <h1 className=' tracking-widest text-gray-400 text-xl font-RighteousFont absolute top-3 left-[40%]'>interval</h1>
-            </nav>
-        )
-    }
+            }
+        </AnimatePresence>
+    )
+
+    // if (menuOpen){
+    //     return (
+    //         <motion.nav 
+    //             className='menu-open min-w-[375px] h-full bg-gray-900 grid items-center justify-center absolute'
+    //             animate='open'
+    //             initial='closed'
+    //             exit='closed'
+    //             variants={menuVariants}
+    //             >
+    //             <img src={LogoHorizontalWhite} onClick={toggleMenu}alt="logo" className='w-9 pt-3 ml-4 absolute top-3' />
+    //             <ul className=' font-PTSans tracking-widest font-bold text-2xl text-gray-50 flex flex-col gap-8 '>
+    //                 <li>ANALOG TIMER</li>
+    //                 <li>DIGITAL TIMER</li>
+    //                 <li>VISUAL TIMER</li>
+    //                 <li>TEXT TIMER</li>
+    //                 <li>CIRCLES TIMER</li>
+    //             </ul>
+    //         </motion.nav>
+    //     )
+    // } else {
+    //     return (
+    //         <motion.nav 
+    //             className='menu-closed min-w-[375px] bg-gray-50 shadow-2xl absolute top-3'
+    //             animate='open'
+    //             initial='closed'
+    //             exit='closed'
+    //             variants={menuVariants}
+    //             >
+    //             <img src={LogoHorizontalBlack} onClick={toggleMenu} alt="logo" className='w-9 pt-3  ml-4 absolute ' />
+    //             <h1 className=' tracking-widest text-gray-400 text-xl font-RighteousFont absolute top-3 left-[40%]'>interval</h1>
+    //         </motion.nav>
+    //     )
+    // }
 
 }
--- src/pages/SetTimer.jsx
+++ src/pages/SetTimer.jsx
@@ -2,10 +2,13 @@ import Menu from '../components/Menu'
 import ArrowLeft from '../assets/arrowLeft.svg'
 import ArrowRight from '../assets/arrowRight.svg'
 import {useState} from 'react'
+import useTimer from 'easytimer-react-hook'
+import {motion} from 'framer-motion'
 
 export default function SetTimerPage(){
 
     const [time, setTime] = useState(10)
+    const [timer, isTargetAchieved] = useTimer()
 
     const handleIncrement = () => {
         setTime(time + 5)
@@ -17,26 +20,64 @@ export default function SetTimerPage(){
         }
     }
 
+    const handleStart = () => {
+        timer.start({precision: 'seconds', target: {minutes: time}})
+    }
+
+    if(isTargetAchieved){
+        console.log('target achieved');
+    }
+    
+    const remainingTime = timer.getTimeValues().toString()
+    
+    const blinkVariants = {
+        visible: {
+          opacity: 1,
+          transition: { duration: 0.2, ease: "easeInOut" }, // Duration of the blink
+        },
+        hidden: {
+          opacity: 0,
+          transition: { duration: 0.2, ease: "easeInOut" },
+        },
+      };
+
     return (
-        <div className='wrapper w-full min-h-svh grid bg-gray-300'>
+        <motion.div 
+            className='wrapper w-full min-h-svh grid bg-gray-900 text-gray-900'
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 0.01}}
+        >
+            {timer ? <p className='absolute text-white'>{remainingTime}</p> : null}
         <main className='min-w-[375px] mx-auto  bg-gray-50 shadow-2xl flex flex-col items-center justify-center gap-y-16 relative'>
             <Menu />
             <section className='flex gap-[42px] items-center'>
-                <img 
+                <motion.img 
                     src={ArrowLeft} 
                     alt="add five minutes" 
-                    className='left-10' 
+                    className='left-10  hover:cursor-pointer  fill-gray-900 ' 
                     onClick={handleDecrement} 
+                    whileTap={{scale: 0.9}}
                 />
                 <section className='flex flex-col items-center'>
-                    <h1 className=' font-PTSans text-[80px] font-semibold'>{time}</h1>
+                    <motion.h1 
+                        className=' font-PTSans text-[80px] font-semibold'
+                        key={time}
+                        animate="visible"
+                        initial='hidden'
+                        exit='hidden'
+                        variants={blinkVariants}
+                    >
+                        {time}
+                    </motion.h1>
                     <p className=' font-PTSans text-[16px] text-gray-500'>minutes</p>
                 </section>
-                <img 
+                <motion.img 
                     src={ArrowRight} 
                     alt="reduce five minutes" 
-                    className='right-10' 
+                    className='right-10 hover:cursor-pointer  fill-gray-900 ' 
                     onClick={handleIncrement}
+                    whileTap={{scale: 0.9}}
                 />
             </section>
             <section className='flex flex-col gap-y-4'>
@@ -48,9 +89,15 @@ export default function SetTimerPage(){
                     <input type="checkbox" name='break' id='break' />
                     <label htmlFor="break" className='text-gray-500 font-PTSans font-xs tracking-widest'>5 min break / interval</label>
                 </section>
-                <button className='h-[51px] w-[279px] font-PTSans text-[24px] font-semibold tracking-widest border border-gray-900 rounded-md'>START TIMER</button>
+                <motion.button 
+                    className='h-[51px] w-[279px] font-PTSans text-[24px] font-semibold tracking-widest border border-gray-900 rounded-md'
+                    initial={{scale: 1}}
+                    whileTap={{scale: 0.95}}
+                    transition={{duration: 0.1, ease: 'easeIn'}}
+                    onClick={handleStart}
+                >START TIMER</motion.button>
             </section>
         </main>
-      </div>
+      </motion.div>
     )
 }
